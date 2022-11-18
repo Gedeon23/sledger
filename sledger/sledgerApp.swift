931f37c455560258bbabe371d96d9d1d99f32342
Gedeon Jahns <gedeon.jahns@gmail.com>
2022-11-19 00:32:18 +0100

    Initial Commit

---
+++ sledger/sledgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  sledgerApp.swift
+//  sledger
+//
+//  Created by Gedeon Jahns on 19.11.22.
+//
+
+import SwiftUI
+
+@main
+struct sledgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
